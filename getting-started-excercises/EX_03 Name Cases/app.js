fd909df02c0ab602e49160f63f25a155a0b10bd1
Shaikh-Muhammad-Usman-official <shaikhmuhammadusmanofficial@gmail.com>
2024-03-13 10:27:16 +0500

    Assignment 3 out of 45 uploaded

---
+++ getting-started-excercises/EX_03 Name Cases/app.js
@@ -0,0 +1,8 @@
+// Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+// storing person name in variable
+var person_name = 'Tony Stark';
+// converting name in uppercase , and lowercase 
+console.log("Your name in  UpperCase is ", person_name.toUpperCase());
+console.log("Your name in LowerCase is", person_name.toLowerCase());
+// converting name in title case
+console.log('Your Name In Title Case Is', person_name.charAt(0).toUpperCase() + person_name.slice(1).toLowerCase());
