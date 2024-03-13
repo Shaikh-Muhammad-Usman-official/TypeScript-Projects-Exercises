65ebb1ae3663025c837731e791487d81063088d5
Shaikh-Muhammad-Usman-official <shaikhmuhammadusmanofficial@gmail.com>
2024-03-13 11:04:00 +0500

    Assignment 4 out of 45 uploaded

--- getting-started-excercises/Ex04-FamousQuote/main.js
+++ getting-started-excercises/Ex04-FamousQuote/main.js
@@ -1,1 +1,1 @@
-console.log("Albert Einstein once said, “A person who never made a mistake never tried anything new.”");
+console.log("Thomas Edision once said, I have not failed, I've just found 10,000 ways that won't work");
