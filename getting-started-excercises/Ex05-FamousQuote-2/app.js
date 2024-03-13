0ab80288e02bd89e059677ee211f4e51270a378f
Shaikh-Muhammad-Usman-official <shaikhmuhammadusmanofficial@gmail.com>
2024-03-13 10:55:21 +0500

    Assignment 5 out of 45 uploaded

---
+++ getting-started-excercises/Ex05-FamousQuote-2/app.js
@@ -0,0 +1,3 @@
+var famous_person = "Thomas Edision";
+var message = "I have not failed, I've just found 10,000 ways that won't work";
+console.log("".concat(famous_person, " once said, \"").concat(message, "\""));
---
+++ getting-started-excercises/Ex05-FamousQuote-2/app.ts
@@ -0,0 +1,3 @@
+let famous_person : string = "Thomas Edision"
+let message : string = "I have not failed, I've just found 10,000 ways that won't work"
+console.log(`${famous_person} once said, "${message}"`);
