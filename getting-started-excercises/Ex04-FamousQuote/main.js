cde3b4778a2970ac6f9ee8f8749808c61a2d8a1f
Shaikh-Muhammad-Usman-official <shaikhmuhammadusmanofficial@gmail.com>
2024-03-14 11:27:11 +0500

    Assignment 4 of 45 uploaded

--- getting-started-excercises/Ex04-FamousQuote/main.js
+++ getting-started-excercises/Ex04-FamousQuote/main.js
@@ -1,1 +1,1 @@
-console.log("Thomas Edision once said, I have not failed, I've just found 10,000 ways that won't work");
+console.log("Thomas Edison once said, \"I have not failed, I've just found 10,000 ways that won't work\" ");
