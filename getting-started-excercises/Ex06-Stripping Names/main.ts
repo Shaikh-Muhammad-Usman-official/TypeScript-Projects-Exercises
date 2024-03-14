7d9d68f7ee4e510bd4b34160487bf48fddf7b4b3
Shaikh-Muhammad-Usman-official <shaikhmuhammadusmanofficial@gmail.com>
2024-03-14 12:20:36 +0500

    Assignment 6 of 45 uploaded

---
+++ getting-started-excercises/Ex06-Stripping Names/main.ts
@@ -0,0 +1,6 @@
+let myName : string="\t\n Tom Cruise  \t\n"
+// without stripping 
+console.log('Without Stripping My name is ',myName);
+// with stripping
+console.log("With Stripping My name is ",myName.trim());
+
