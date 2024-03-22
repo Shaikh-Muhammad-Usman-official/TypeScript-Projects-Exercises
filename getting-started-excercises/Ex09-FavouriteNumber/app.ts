e761acc8ebb76dbeacec8fab9632d883cdd0fb7b
Shaikh-Muhammad-Usman-official <shaikhmuhammadusmanofficial@gmail.com>
2024-03-22 23:03:41 +0500

    Assignmnet 09 of 45 uploaded

---
+++ getting-started-excercises/Ex09-FavouriteNumber/app.ts
@@ -0,0 +1,4 @@
+// Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+let favourite_num = 0 ;
+let message = `Your Favourite Number Is: ${favourite_num}`;
+console.log(message);
